f3523cd580e3db6546e65eb1c7daceb5c4b67b3c
LeoLuZiYuan <asd88551144@gmail.com>
2020-04-10 07:25:07 +0800

    2020/04/10

---
+++ class18-jQuery/excercise/selector/JqueryExcer.js
@@ -0,0 +1,7 @@
+console.log("connect!");
+$("div").css("backgroundColor", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", " 3px solid orange");
+// $("div")[0].style.color = "pink";
+$("div:first-of-type").css("color","pink");
+
