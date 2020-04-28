b2e1acd059faa3eb6db5f96251aa307732645b96
LeoLuZiYuan <asd88551144@gmail.com>
2020-04-28 09:08:24 +0800

    2020/04/28

--- class18-jQuery/TodoListProject/assets/js/todos.js
+++ class18-jQuery/TodoListProject/assets/js/todos.js
@@ -1,1 +1,22 @@
-console.log("connect");
+//checkout  Off specific Todos By clicking
+$("li").click(function(){
+    //$(this).css("color","gray");
+    //$(this).css("text-decoration","line-through");
+    //if li is gray
+    console.log($(this).css("color"))
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        //turn it black
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    }
+    else{
+          //turn it gray
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+      
+})
